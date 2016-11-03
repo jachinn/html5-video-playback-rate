d6f9d5bdd737c183546ed8aef159db165502c2f6
Janis <janis.chinn+github@gmail.com>
2016-11-03 10:47:32 -0700

    Include current playback rate in prompt.
    Prevent setting playback rate to 0.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var media = document.querySelectorAll('video, audio');
-var rate = prompt('Speed?','');
+var rate = prompt('Speed? Currently playing back at ' + media[0].playbackRate + 'x.','');
 Array.prototype.forEach.call(media, function(player) {
-   player.playbackRate = rate;
+   if (rate != 0) {
+      player.playbackRate = rate;
+   } else {
+      player.playbackRate = 1;
+   }
 });
