24111a6cad56a5c2bd6954a7ae3fbb340e6ef871
Janis <janis.chinn+github@gmail.com>
2016-11-04 14:39:07 -0700

    Maintain current speed if user cancels out.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var media = document.querySelectorAll('video, audio');
 var rate = prompt('Speed? Currently playing back at ' + media[0].playbackRate + 'x.','');
 Array.prototype.forEach.call(media, function(player) {
-   if (rate != 0) {
+   if (rate == null) {
+      return;
+   } else if (rate != 0) {
       player.playbackRate = rate;
    } else {
       player.playbackRate = 1;
